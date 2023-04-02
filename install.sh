e119e85eb3b88526245a2ccbd39862b5df806168
maxstothart <99781973+maxstothart@users.noreply.github.com>
2023-04-02 17:19:43 +1200

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,8 +5,8 @@ sudo apt install apache2
 sudo apt install mysql-server
 sudo mysql_secure_installation
 sudo apt install php libapache2-mod-php php-mysql
-sudo cp homeserver.conf /etc/apache2/sites-enabled/homeserver.conf
+sudo cp homeserver.conf /etc/apache2/sites-available/homeserver.conf
 sudo a2enmod proxy proxy_http
 sudo a2dissite 000-default
-sudo a2ensite homeserver
+sudo a2ensite homeserver.conf
 sudo systemctl restart apache2
